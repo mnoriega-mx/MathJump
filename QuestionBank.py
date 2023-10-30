1fca77022828dd61492957af3cc336fe7144f9f9
MauricioNoriegaTec <110953315+MauricioNoriegaTec@users.noreply.github.com>
2023-10-30 15:55:40 -0600

    first commit

---
+++ QuestionBank.py
@@ -0,0 +1,213 @@
+def get_question(question_num):
+
+  if question_num == 1:
+    question = {
+      "question": "x^2-9=0",
+      "correct": "x=3, x=-3",
+      "incorrect": "x=3, x=9"
+    }
+
+  if question_num == 2:
+    question = {
+      "question": "x^2+2x=0",
+      "correct": "x=0, x=-2",
+      "incorrect": "x=0, x=2"
+    }
+    
+  if question_num == 3:
+    question = {
+      "question": "sin(x)/cos(x)",
+      "correct": "tan(x)",
+      "incorrect": "cot(x)"
+    }
+    
+  if question_num == 4:
+    question = {
+      "question": "1/sin(x)",
+      "correct": "csc(x)",
+      "incorrect": "sec(x)"
+    }
+    
+  if question_num == 5:
+    question = {
+      "question": "x^2-2x-8=0",
+      "correct": "x=-2, x=4",
+      "incorrect": "x=-2, x=-4"
+    }
+    
+  if question_num == 6:
+    question = {
+      "question": "3^4/3^2",
+      "correct": 9,
+      "incorrect": 12
+    }
+    
+  if question_num == 7:
+    question = {
+      "question": "5^0:",
+      "correct": 1,
+      "incorrect": 0
+    }
+    
+  if question_num == 8:
+    question = {
+      "question": "What is m in y=mx+b",
+      "correct": "Slope",
+      "incorrect": "x-intercept"
+    }
+    
+  if question_num == 9:
+    question = {
+      "question": "5x(x+8)=0",
+      "correct": "x=0,x=-8",
+      "incorrect": "x=-1,x=8"
+    }
+    
+  if question_num == 10:
+    question = {
+      "question": "One radian in degrees",
+      "correct": 57.29,
+      "incorrect": 60.23
+    }
+    
+  if question_num == 11:
+    question = {
+      "question": "√196",
+      "correct": 14,
+      "incorrect": 18
+    }
+
+  if question_num == 12:
+    question = {
+      "question": "√-1",
+      "correct": "Imaginary number",
+      "incorrect": "√-2"
+    }
+    
+  if question_num == 13:
+    question = {
+      "question": "y-intercept for sin(x)",
+      "correct": "0",
+      "incorrect": "1"
+    }
+    
+  if question_num == 14:
+    question = {
+      "question": "y-intercept for cos(x)",
+      "correct": 1,
+      "incorrect": 0
+    }
+    
+  if question_num == 15:
+    question = {
+      "question": "√-4",
+      "correct": 'Imaginary number',
+      "incorrect": -2
+    }
+    
+  if question_num == 16:
+    question = {
+      "question": "18% de 50:",
+      "correct": 9,
+      "incorrect": 18
+    }
+    
+  if question_num == 17:
+    question = {
+      "question": "Select the prime number",
+      "correct": 17,
+      "incorrect": 51
+    }
+    
+  if question_num == 18:
+    question = {
+      "question": "Next number in 2,4,16...",
+      "correct": "256",
+      "incorrect": "64"
+    }
+    
+  if question_num == 19:
+    question = {
+      "question": "x^-6",
+      "correct": "1/x^6",
+      "incorrect": "Imaginary number"
+    }
+    
+  if question_num == 20:
+    question = {
+      "question": "Derivative of 26",
+      "correct": "0",
+      "incorrect": "26x"
+    }
+    
+  if question_num == 21:
+    question = {
+      "question": "(x+3)^2=49",
+      "correct": "x=-10,x=4",
+      "incorrect": "x=-10,x=4"
+    }
+
+  if question_num == 22:
+    question = {
+      "question": "4!",
+      "correct": 24,
+      "incorrect": 100
+    }
+    
+  if question_num == 23:
+    question = {
+      "question": "Anti-derivartve of 30x",
+      "correct": "15x^2",
+      "incorrect": "30x/2"
+    }
+    
+  if question_num == 24:
+    question = {
+      "question": "Derivative of e^2x",
+      "correct": '2e^2x',
+      "incorrect": 'e^2x'
+    }
+    
+  if question_num == 25:
+    question = {
+      "question": "4^2/4^1",
+      "correct": 4,
+      "incorrect": 16
+    }
+    
+  if question_num == 26:
+    question = {
+      "question": "Derivative of ln(5x)",
+      "correct": "1/x",
+      "incorrect": "5x^2/5x"
+    }
+    
+  if question_num == 27:
+    question = {
+      "question": "If tanA 2/3, sinA=",
+      "correct": "2√13",
+      "incorrect": "3√14"
+    }
+    
+  if question_num == 28:
+    question = {
+      "question": "Tangent",
+      "correct": "Opposite/Adjacent",
+      "incorrect": "Opposite/Hypotenuse"
+    }
+    
+  if question_num == 29:
+    question = {
+      "question": "Derivative of 4x^2+5x+20",
+      "correct": "8x+5",
+      "incorrect": "6x^2+2"
+    }
+    
+  if question_num == 30:
+    question = {
+      "question": "Derivative of 3x+2",
+      "correct": 3,
+      "incorrect": 2
+    }
+   
+  return question
